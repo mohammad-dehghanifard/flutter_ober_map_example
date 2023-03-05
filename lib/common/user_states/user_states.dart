ec1c5332c3d8d100fb2766c13c18ccc28d6e643d
mohammad-dehghanifard <m.deheghanifard@gmail.com>
2023-03-05 00:03:09 -0800

    implemented ui states

---
+++ lib/common/user_states/user_states.dart
@@ -0,0 +1,6 @@
+class UserStates{
+  UserStates._();
+  static const selectedOrigin = 0;
+  static const selectedDestination = 1;
+  static const requestDriver = 2;
+}
--- lib/ui/map_screen.dart
+++ lib/ui/map_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map_example/common/dimens/dimens.dart';
 import 'package:flutter_map_example/common/text_style/text_style.dart';
+import 'package:flutter_map_example/common/user_states/user_states.dart';
 import 'package:flutter_map_example/common/widget/back_btn_widget.dart';
 
 class MapScreen extends StatefulWidget {
@@ -11,6 +12,7 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  List userCurrentState = [UserStates.selectedOrigin];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,27 +22,86 @@ class _MapScreenState extends State<MapScreen> {
             // osm map
             Container(color: Colors.grey.shade300),
             // btn
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Padding(
-                padding: const EdgeInsets.all(AppDimens.large),
-                child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text("انتخاب مبدا",style: AppTextStyle.btnTxtStyle)),
-              ),
-            ),
+            currentState(),
             // back btn
             Positioned(
               top: AppDimens.small,
               left: AppDimens.medium,
-              child: MapBackBtn(onTap: () {}),
+              child: MapBackBtn(onTap: () {
+                if(userCurrentState.length > 1){
+                  setState(() {
+                    userCurrentState.removeLast();
+                  });
+                }
+              }),
             )
           ],
         ),
       ),
     );
+  }
+
+  Positioned originState() {
+    return Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Padding(
+              padding: const EdgeInsets.all(AppDimens.large),
+              child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      userCurrentState.add(UserStates.selectedDestination);
+                    });
+                  },
+                  child: Text("انتخاب مبدا",style: AppTextStyle.btnTxtStyle)),
+            ),
+          );
+  }
+  Positioned destinationState() {
+    return Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Padding(
+              padding: const EdgeInsets.all(AppDimens.large),
+              child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      userCurrentState.add(UserStates.requestDriver);
+                    });
+                  },
+                  child: Text("انتخاب مقصد",style: AppTextStyle.btnTxtStyle)),
+            ),
+          );
+  }
+  Positioned reqDriverState() {
+    return Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Padding(
+              padding: const EdgeInsets.all(AppDimens.large),
+              child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text("درخواست راننده",style: AppTextStyle.btnTxtStyle)),
+            ),
+          );
+  }
+
+  Widget currentState(){
+    Widget widget = originState();
+    switch(userCurrentState.last){
+      case UserStates.selectedOrigin :
+        widget = originState();
+        break;
+      case UserStates.selectedDestination :
+        widget = destinationState();
+        break;
+      case UserStates.requestDriver :
+        widget = reqDriverState();
+    }
+    return widget;
   }
 }
 
