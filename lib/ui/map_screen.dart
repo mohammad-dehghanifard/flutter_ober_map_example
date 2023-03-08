05c49520da39f89f2d7ecdf9c4c4cb892e0ff844
mohammad-dehghanifard <m.deheghanifard@gmail.com>
2023-03-07 22:27:05 -0800

    insert origin and destination and calculate distance

--- lib/ui/map_screen.dart
+++ lib/ui/map_screen.dart
@@ -15,9 +15,12 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  List userCurrentState = [UserStates.selectedOrigin];
-  List<GeoPoint> geoPoint = []; /// map points
-  Widget markIcon = SvgPicture.asset(Assets.images.origin,height: 100,width: 40); // map icon marker
+  List userCurrentState = [UserStates.selectedOrigin]; // current state
+  List<GeoPoint> geoPoint = []; // map points
+  String distance = 'در حال محسابه فاصله تا مقصد...';
+  Widget iconMarker = SvgPicture.asset(Assets.images.origin,height: 100,width: 40); /// map icon marker
+  Widget originMarker = SvgPicture.asset(Assets.images.origin,height: 100,width: 40);
+  Widget destinationMarker = SvgPicture.asset(Assets.images.destination,height: 100,width: 40);
 
   //map controller
   MapController mapController = MapController(
@@ -38,7 +41,7 @@ class _MapScreenState extends State<MapScreen> {
                 trackMyPosition: true,
                 isPicker: true,
                 mapIsLoading: const Center(child:  CircularProgressIndicator()),
-                markerOption: MarkerOption(advancedPickerMarker: MarkerIcon(iconWidget: markIcon)),
+                markerOption: MarkerOption(advancedPickerMarker: MarkerIcon(iconWidget: iconMarker)),
                 initZoom: 15,
                 minZoomLevel: 8,
                 maxZoomLevel: 18,
@@ -52,6 +55,11 @@ class _MapScreenState extends State<MapScreen> {
               top: AppDimens.small,
               left: AppDimens.medium,
               child: MapBackBtn(onTap: () {
+                if(geoPoint.isNotEmpty){
+                  geoPoint.removeLast();
+                  iconMarker = SvgPicture.asset(Assets.images.origin,height: 100,width: 40);
+                  mapController.init();
+                }
                 if(userCurrentState.length > 1){
                   setState(() {
                     userCurrentState.removeLast();
@@ -74,10 +82,10 @@ class _MapScreenState extends State<MapScreen> {
               padding: const EdgeInsets.all(AppDimens.large),
               child: ElevatedButton(
                   onPressed: () async {
-                    /// get origin location point
+                    // get origin location point
                     GeoPoint originGeoPoint = await mapController.getCurrentPositionAdvancedPositionPicker();
                     geoPoint.add(originGeoPoint);
-                    markIcon = SvgPicture.asset(Assets.images.destination);
+                    iconMarker = destinationMarker;
                     setState(() {
                       userCurrentState.add(UserStates.selectedDestination);
                     });
@@ -95,7 +103,27 @@ class _MapScreenState extends State<MapScreen> {
             child: Padding(
               padding: const EdgeInsets.all(AppDimens.large),
               child: ElevatedButton(
-                  onPressed: () {
+                  onPressed: () async {
+                    // add destination point to list
+                    await mapController.getCurrentPositionAdvancedPositionPicker().then((value) {
+                      geoPoint.add(value);
+                    });
+                    // mark origin and destination to map
+                    mapController.cancelAdvancedPositionPicker();
+                    mapController.addMarker(geoPoint.first,markerIcon: MarkerIcon(iconWidget:originMarker));
+                    mapController.addMarker(geoPoint.last,markerIcon: MarkerIcon(iconWidget: destinationMarker));
+                    //ToDo : مارکر نقشه باگ داره و تو هر دو نقطه مارکر مبدا رو قرار میده
+
+                   // calculate distance
+                   await distance2point(geoPoint.first, geoPoint.last).then((value) {
+                     setState(() {
+                       if(value <= 1000){
+                         distance = "فاصله تا مقصد ${value.toInt()} متر";
+                       }else{
+                         distance = "فاصله تا مقصد ${value.toInt()} کیلو متر";
+                       }
+                     });
+                   });
                     setState(() {
                       userCurrentState.add(UserStates.requestDriver);
                     });
@@ -105,15 +133,34 @@ class _MapScreenState extends State<MapScreen> {
           );
   }
   Widget reqDriverState() {
+    mapController.zoomOut();
     return Positioned(
             bottom: 0,
             left: 0,
             right: 0,
             child: Padding(
               padding: const EdgeInsets.all(AppDimens.large),
-              child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text("درخواست راننده",style: AppTextStyle.btnTxtStyle)),
+              child: Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 58,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Center(child: Text(distance)),
+                  ),
+                  const SizedBox(height: AppDimens.medium),
+                  //request driver btn
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text("درخواست راننده",style: AppTextStyle.btnTxtStyle)),
+                  ),
+                ],
+              ),
             ),
           );
   }
